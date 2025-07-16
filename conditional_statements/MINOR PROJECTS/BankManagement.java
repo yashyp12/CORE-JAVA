fe16b80a347a5683214d4d9159181d6bdf91c2d4
Yash Patil <patilyashyp12@gmail.com>
2025-07-16 14:11:28 +0530

    added program for electricity bill generator

--- conditional_statements/MINOR PROJECTS/BankManagement.java
+++ conditional_statements/MINOR PROJECTS/BankManagement.java
@@ -10,6 +10,7 @@ public class BankManagement {
     static int pin;
     static String username;
 
+    
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
@@ -22,11 +23,9 @@ public class BankManagement {
 
                 case 1: {
 
-                    if (name == null) {
-                        System.out.println("Invalid user please Create your Acoount");
-                        continue;
-                    } 
+                   
 
+                    // Login Validation
                         System.out.println("login");
                         sc.nextLine();
                         System.out.println("Enter your username");
@@ -35,12 +34,18 @@ public class BankManagement {
                         System.out.println("Enter your pin");
                         int pin1 = sc.nextInt();
                       
+                        // validation error message
+                         if (name == null) {
+                        System.out.println("Invalid user please Create your Acoount");
+                        continue;
+                    } 
 
 
                         if (pin1 == pin && user1.equals(username)) {
 
                             System.out.println("Login success !");
 
+                            // bank operations
                             for (;;) {
                                 System.out.println("OPERATIONS : 1.DEPOSIT, 2.WITHDRAW , 3.BALANCE 4.Logout 5.Account Info");
                                 int userinp1 = sc.nextInt();
@@ -72,7 +77,7 @@ public class BankManagement {
                                             }
                                         }else{
                                             System.out.println("invalid PIN");
-                                            continue;
+                                           
                                         }
 
                                         break; 
@@ -83,9 +88,9 @@ public class BankManagement {
                                         System.out.println("Enter your PIN ");
                                         int pin3 = sc.nextInt();
                                         if(pin3 == pin){
-                                            System.out.println("Availaible Balance " + balance);
                                             System.out.println("balance fethced successfully");
-
+                                            System.out.println("Availaible Balance " + balance);
+                                            
                                         }
                                         else{
                                             System.out.println("invalid pin");
@@ -96,7 +101,17 @@ public class BankManagement {
 
                                     case 4: {
                                         System.out.println("you are logout");
-                                        return;
+                                     return;
+                                       
+                                    }
+
+
+                                    case 5:{
+                                        System.out.println("Showing your Account Info !");
+                                        System.out.println("Customer Name is " + name);
+                                        System.out.println("Customer ACCOUNT NUMBER IS" + Account_Number);
+                                        System.out.println("Customer username is  " + username);
+                                        break;
                                     }
 
                                     default: {
@@ -106,6 +121,10 @@ public class BankManagement {
 
                             }
 
+                        }
+                        else{
+                            System.out.println("invalid pin Entered Try Again !!");
+                            continue;
                         }
 
  
@@ -119,16 +138,15 @@ public class BankManagement {
                     System.out.println("");
                     System.out.println("Set your PIN");
                     pin = sc.nextInt();
-                    System.out.println("");
                     System.out.println("Enter your Name");
                     name = sc.nextLine();
                     System.out.println("Acoount Created succesfully Pleases Login");
-                     
+                     break;
                 }
 
                 default:{
                     System.out.println("invalid Credentials ");
-                    continue;
+                   
                 }
             }
         }
