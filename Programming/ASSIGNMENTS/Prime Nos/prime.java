96bbb37f81c4fa1858d1d19775c5599ca9b52717
Yash Patil <patilyashyp12@gmail.com>
2025-09-27 23:01:13 +0530

    added for prime num

---
+++ Programming/ASSIGNMENTS/Prime Nos/prime.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+class prime {
+
+    public static void main(String[] args) {
+
+        System.out.println("enter a number");
+        int num = new Scanner(System.in).nextInt();
+        int den = 2;
+
+        // check whether the num is prime or not
+        while (den <= num / 2) {
+
+            if (num % den == 0) {
+                break;
+            }
+            den++;
+        }
+
+        if (den > num / 2) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not an prime");
+        }
+
+    }
+}
