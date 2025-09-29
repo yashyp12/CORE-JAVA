7f41e73ccfe49031905277523f4e5392e7d7ebff
Yash Patil <patilyashyp12@gmail.com>
2025-09-29 19:44:12 +0530

    Comment out code in Kuber.java and UberDriver.java
    All code in Kuber.java and UberDriver.java has been commented out, effectively disabling their functionality. Additionally, project configuration files for IntelliJ IDEA were added, along with compiled class files and module files in the output directory.

--- PROJECTS/Kuber.java
+++ PROJECTS/Kuber.java
@@ -1,103 +1,103 @@
-class Driver{
-//gives driver details
-private String name;
-private Long contact;
-
-Driver(String name,Long contact){
-	this.name = name;
-	this.contact = contact;
-}
-
-//getter for fetching driver details
-public void getDriver(){
-	System.out.println("\n Driver Info");
-	System.out.println("Driver Name " + name);
-	System.out.println("Contact : " + contact);
-}
-
-}
-
-
-class Cab{
-
-	static int id = 1212;
-	private Driver driver;
-	private String number;
-	private String uberId;
-	private String type;
-	private int capacity;
-	private String status = "AVAIL";
-	private String location = "DECCAN";
-
-	String [] typeVehicle = {"hatchback","sedan","suv","muv"};
-	int [] vehicleCapacity = {3,4,5,6};
-
-	{
-		//this will give an unqieu id executes first when obj created
-		this.uberId = "UBER" + id++;
-	}
-
-Cab(String number,String type,Driver driver){
-
-	this.number = number;
-	this.type = type.toLowerCase();
-
-	int indx = 0;
-	for(String element: typeVehicle){
-		if(type.toLowerCase().equals(element){
-			break;
-		}
-		indx++;
-	}
-
-	this.capacity = vehicleCapacity[indx];
-	this.driver = driver;
-
-}
-
-
-public void getCabDetails(){
-	System.out.println("\N CAB DETAILS");
-	System.out.println("Uber Id : " + uberId);
-	System.out.println("Status " + status);
-	System.out.println("Type :" + type);
-	System.out.println("Capacity: " + capacity);
-	System.out.println("Location " + location);
-	System.out.println("");
-}
-
-public String getCabNumber(){
-	return number;
-}
-
-public Driver getDriver(){
-	return driver;
-}
-
-public String getStatus(){
-	return status;
-}
-
-public void setStatus(String newStatus){
-	status = newStatus;
-}
-
-public int getCapacity(){
-	return capacity;
-}
-
-public String getUberId(){
-	return uberId;
-}
-
-}
-
-
-
-
-class Kuber{
-
-	public static void main(String[] args) {
-		System.out.println("Welcome into Kuber");
-	}
-}
+//class Driver{
+////gives driver details
+//private String name;
+//private Long contact;
+//
+//Driver(String name,Long contact){
+//	this.name = name;
+//	this.contact = contact;
+//}
+//
+////getter for fetching driver details
+//public void getDriver(){
+//	System.out.println("\n Driver Info");
+//	System.out.println("Driver Name " + name);
+//	System.out.println("Contact : " + contact);
+//}
+//
+//}
+//
+//
+//class Cab{
+//
+//	static int id = 1212;
+//	private Driver driver;
+//	private String number;
+//	private String uberId;
+//	private String type;
+//	private int capacity;
+//	private String status = "AVAIL";
+//	private String location = "DECCAN";
+//
+//	String [] typeVehicle = {"hatchback","sedan","suv","muv"};
+//	int [] vehicleCapacity = {3,4,5,6};
+//
+//	{
+//		//this will give an unqieu id executes first when obj created
+//		this.uberId = "UBER" + id++;
+//	}
+//
+//Cab(String number,String type,Driver driver){
+//
+//	this.number = number;
+//	this.type = type.toLowerCase();
+//
+//	int indx = 0;
+//	for(String element: typeVehicle){
+//		if(type.toLowerCase().equals(element)){
+//			break;
+//		}
+//		indx++;
+//	}
+//
+//	this.capacity = vehicleCapacity[indx];
+//	this.driver = driver;
+//
+//}
+//
+//
+//public void getCabDetails(){
+//	System.out.println(" CAB DETAILS");
+//	System.out.println("Uber Id : " + uberId);
+//	System.out.println("Status " + status);
+//	System.out.println("Type :" + type);
+//	System.out.println("Capacity: " + capacity);
+//	System.out.println("Location " + location);
+//	System.out.println("");
+//}
+//
+//public String getCabNumber(){
+//	return number;
+//}
+//
+//public Driver getDriver(){
+//	return driver;
+//}
+//
+//public String getStatus(){
+//	return status;
+//}
+//
+//public void setStatus(String newStatus){
+//	status = newStatus;
+//}
+//
+//public int getCapacity(){
+//	return capacity;
+//}
+//
+//public String getUberId(){
+//	return uberId;
+//}
+//
+//}
+//
+//
+//
+//
+//class Kuber{
+//
+//	public static void main(String[] args) {
+//		System.out.println("Welcome into Kuber");
+//	}
+//}
