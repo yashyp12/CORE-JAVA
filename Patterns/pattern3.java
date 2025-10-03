6a4ec5b2909638509acfae556505a4564e9196d1
Yash Patil <patilyashyp12@gmail.com>
2025-10-03 21:29:13 +0530

    Add pattern3 Java program for printing a decreasing star pattern

---
+++ Patterns/pattern3.java
@@ -0,0 +1,20 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        /*
+         *    * * * * 
+         *    * * *
+         *    * *
+         *    *
+         */
+
+        int n =  4;
+        
+        for(int row =1; row<=n; row++){
+
+            for(int col = 1; col<=n+1-row; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
