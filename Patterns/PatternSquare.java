aff8c851bd1b1f6904ef72db2089d7143c08c2c8
Yash Patil <patilyashyp12@gmail.com>
2025-10-03 22:35:47 +0530

    Add PatternSquare Java program for printing a square pattern

---
+++ Patterns/PatternSquare.java
@@ -0,0 +1,28 @@
+public class PatternSquare {
+    public static void main(String[] args) {
+        
+        /*
+         *    * * * * 
+         *    *     *
+         *    *     *
+         *    * * * *
+         */
+
+         int n = 4;
+
+         for(int row = 1; row<=n; row++){
+
+            for(int col = 1; col<=n; col++)
+            {
+                if(row==1 || row ==n || col==1 || col==n)
+                {
+                    System.out.print("*");
+                }else{
+
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); //next row
+         }
+    }
+}
