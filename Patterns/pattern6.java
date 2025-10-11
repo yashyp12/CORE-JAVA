971e5fde1cfbb8a99c92a72adff0138f3c58c772
Yash Patil <patilyashyp12@gmail.com>
2025-10-11 22:46:45 +0530

    Remove unused classes and commented-out code from prime, var1, PalindromeNum, and ReverString

--- Patterns/pattern6.java
+++
@@ -1,7 +0,0 @@
-public class pattern6 {
-public static void main(String[] args) {
-    
-    
-
-}
-}
--- Programming/ASSIGNMENTS/Prime Nos/prime.java
+++ Programming/ASSIGNMENTS/Prime Nos/prime.java
@@ -10,19 +10,22 @@ class prime {
         int den = 2;
 
         // check whether the num is prime or not
-        while (den <= num / 2) {
+        // while (den <= num / 2) {
 
-            if (num % den == 0) {
-                break;
-            }
-            den++;
-        }
+        //     if (num % den == 0) {
+        //         break;
+        //     }
+        //     den++;
+        // }
 
-        if (den > num / 2) {
-            System.out.println("prime");
-        } else {
-            System.out.println("not an prime");
-        }
+        // if (den > num / 2) {
+        //     System.out.println("prime");
+        // } else {
+        //     System.out.println("not an prime");
+        // }
+
+
+        
 
     }
 }
