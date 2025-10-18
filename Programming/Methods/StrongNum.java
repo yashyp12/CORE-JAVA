02d929ceb5dc64f058a783b6c6d44395edcbdf36
Yash Patil <patilyashyp12@gmail.com>
2025-10-18 21:57:37 +0530

    added strong num

--- Programming/Methods/StrongNum.java
+++ Programming/Methods/StrongNum.java
@@ -2,42 +2,71 @@
 public class StrongNum {
 
     // sum of factorial of individaula digit
-    // 145 - 1 facto + 4 facto + 5 facto -> if 1+4+5 is same num is strong num
+
+    /*
+     * A strong number is a positive integer where the sum of the factorials of its individual digits equals the original number
+     * ex 145 -> factorial of 1 , 4 , 5 is equals -> 145 (1! + 4! + 5! = 1 + 24 + 120 = 145
+)
+     */
+//     
+//     public static void main(String[] args) {
+//
+//        // rangeOfStrong(1, 45000);
+//
+//    }
+    // public static void rangeOfStrong(int start, int end) {
+    //     while (start <= end) {
+    //         if (isStrong(start)) {
+    //             System.out.println(start);
+    //         }
+    //         start++;
+    //     }
+    // }
+    // public static boolean isStrong(int num) {
+    //     int sum = 0;
+    //     for (int i = num; i != 0; i /= 10) {
+    //         sum = sum + factorial(1 % 10);
+    //     }
+    //     return sum == num;
+    // }
+    // public static int factorial(int num) {
+    //     int fact = 1;
+    //     while (num > 0) {
+    //         fact *= num;
+    //         num--;
+    //     }
+    //     return fact;
+    // }
+    // ***************
     public static void main(String[] args) {
 
-        rangeOfStrong(1, 45000);
-
+        // System.out.println(factorials(5));
+        System.out.println(isStrong(145));
     }
 
-    public static void rangeOfStrong(int start, int end) {
-        while (start <= end) {
-            if (isStrong(start)) {
-                System.out.println(start);
-            }
-            start++;
-        }
-    }
+// step 2 - isStrong num
+    public static boolean isStrong(int num){
 
-    public static boolean isStrong(int num) {
-
-        int sum = 0;
-
-        for (int i = num; i != 0; i /= 10) {
-            sum = sum + factorial(1 % 10);
+        int sum  = 0;
+        
+        for(int i = num; i!=0; i/=10){
+            sum += factorials(i%10);
         }
 
         return sum == num;
     }
 
-    public static int factorial(int num) {
+    // step1 - method for factorial
+    public static int factorials(int num) {
+
         int fact = 1;
 
         while (num > 0) {
             fact *= num;
             num--;
         }
-
         return fact;
 
     }
+
 }
