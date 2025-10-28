adecb8c34dcc8880ff495456cd5c2c35aab04a68
Yash Patil <patilyashyp12@gmail.com>
2025-10-28 07:54:10 +0530

    added animal classs

--- OOPS/Interface/AnimalInterface/Dog.java
+++ OOPS/Interface/AnimalInterface/Dog.java
@@ -1,4 +1,14 @@
 package Interface.AnimalInterface;
 
-public class Dog {
+public class Dog implements Animal{
+
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+
+    public void sleep(){
+        System.out.println("Dog is sleeping");
+    }
+
 }
