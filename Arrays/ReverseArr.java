d8a34161191ae9138341048cef21215d79c17551
Yash Patil <patilyashyp12@gmail.com>
2025-10-31 14:19:29 +0530

    Add array reversal utility using two-pointer method
    Introduces ReverseArr.java, which provides a method to reverse an integer array in place using the two-pointer technique. The main method demonstrates reversing a sample array and printing the result.

---
+++ Arrays/ReverseArr.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class ReverseArr {
+
+    public static void main() {
+        int[] arr = {1, 2, 36, 5, 9};
+//    swap(arr,0,4);
+        reverseARR(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    //this will work for even num arrays and odd num arrays
+//    THIS IS CALLED TWO POINTER METHOD
+    // remember here we are passing the reference of array so it will modifies the original array
+    //In Java, when you pass a primitive type (like int, char) to a method, the method works on a copy of the value.
+// But when you pass an object (like an array or an ArrayList), you are passing a reference to the original object in memory.
+//    Both the main() method and the reverseARR() method are looking at and changing the exact same array in memory.
+    static void reverseARR(int[] arr) {
+//        WE ARE TAKING INDEX FROM 0
+        int start = 0;
+        // we are taking index val so we need to use length-1
+        int end = arr.length - 1;
+
+        while (start < end) {
+            //swap
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+
+    static void swap(int[] arr, int index1, int index2) {
+        Swap.swap(arr, index1, index2);
+    }
+
+}
