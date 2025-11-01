c93512adc3ed3a7cb13bd902455e3b916727a1be
Yash Patil <patilyashyp12@gmail.com>
2025-11-01 10:45:18 +0530

    added palindrome string

---
+++ Strings/palindroString.java
@@ -0,0 +1,24 @@
+public class palindroString {
+
+    public static void main(String[] args) {
+        String str = "abcdcba";
+
+        System.out.println(isPalindrome(str));
+
+    }
+
+    static boolean isPalindrome(String str) {
+        str = str.toLowerCase();
+        // here the lenght is a function but in the array its a variable
+
+        for (int i = 0; i <= str.length() / 2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length() - 1 - i);
+
+            if (start != end)
+                return false; // this means the satrt end not same
+            // for obivous reason the string is not same
+        }
+        return true;
+    }
+}
