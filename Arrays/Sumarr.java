ca16fb1c10869023d98afad9a6015442da1d21d2
Yash Patil <patilyashyp12@gmail.com>
2025-11-05 17:01:55 +0530

    Add Sumarr program to calculate array sum
    Introduced Sumarr.java, which reads five integers into an array, computes their sum, and prints both the array and the sum. Includes corresponding compiled class files.

---
+++ Arrays/Sumarr.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sumarr {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println("sum of array is " + sum);
+    }
+}
