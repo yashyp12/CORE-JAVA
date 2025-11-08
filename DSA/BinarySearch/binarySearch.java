fecaff98ba2e6230aceab3902bbbda70149e40fd
Yash Patil <patilyashyp12@gmail.com>
2025-11-08 10:49:11 +0530

    Add detailed comments to binarySearch.java
    Expanded comments in binarySearch.java to clarify the binary search algorithm steps, including pointer usage, middle calculation, and array traversal logic for ascending order arrays.

--- DSA/BinarySearch/binarySearch.java
+++ DSA/BinarySearch/binarySearch.java
@@ -8,7 +8,21 @@ public class binarySearch {
             // else search in left 
 // step 3 - if middle element  == target elemt // ans
 
+        // if start > end : element not found 
+
 //- used on sorted array either in ascending or descening order
+
+// take 2 pointer start and end  (ascendig order )
+        //find the middle = start + end /2
+        // check if targer > or < middle or == target element
+        // then choose whterh the right or left array
+        // if middle == targer return ans
+        //we are not creating new array we are just changin the start and end point
+
+        // if it lies in right side of arr the star = middle +1;
+        // if it lies in left side the nonly end wil change end = middle -1;
+
+
 
 
 
