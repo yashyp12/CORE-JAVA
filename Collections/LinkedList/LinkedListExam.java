78f661788b31f9d93d2e2b4596761ab878298088
Yash Patil <patilyashyp12@gmail.com>
2025-11-09 23:30:55 +0530

    Add LinkedList notes and example Java class
    Added a markdown file with key points about LinkedList and a Java example demonstrating basic usage of LinkedList. These additions provide reference material and a simple code sample for understanding LinkedList behavior.

---
+++ Collections/LinkedList/LinkedListExam.java
@@ -0,0 +1,16 @@
+package Collections.LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListExam {
+
+   public static void main(String[] args) {
+
+       LinkedList<Integer> list = new LinkedList<>();
+
+       list.add(23);
+       System.out.println(list);
+
+
+    }
+}
