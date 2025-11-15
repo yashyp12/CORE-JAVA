f3897b432dc7d8d7b6c23f0262d9439f59aecc47
Yash Patil <patilyashyp12@gmail.com>
2025-11-15 23:11:21 +0530

    added lined list

--- Collections/LinkedList/LinkedListExam.java
+++ Collections/LinkedList/LinkedListExam.java
@@ -7,7 +7,12 @@ public class LinkedListExam {
    public static void main(String[] args) {
  LinkedList<Integer> list3 = new LinkedList<>();
        System.out.println(list3);
-
-
+list3.add(45);
+list3.add(55);
+list3.add(69);
+list3.add(75);
+       System.out.println(list3);
+list3.removeFirst();
+       System.out.println(list3);
     }
 }
