f4942a8bdb31b5c948bc86b6a80499f9e119539a
Yash Patil <patilyashyp12@gmail.com>
2025-11-16 22:08:42 +0530

    Add queue and deque operations to LinkedListExam
    Imported Queue and Deque interfaces and demonstrated additional LinkedList methods such as peek, offer, poll, and pop for queue and stack-like operations.

--- Collections/ListInterface/LinkedList/LinkedListExam.java
+++ Collections/ListInterface/LinkedList/LinkedListExam.java
@@ -1,4 +1,6 @@
 package Collections.ListInterface.LinkedList;
+import java.util.Queue;
+import java.util.Deque;
 
 import java.util.LinkedList;
 
@@ -15,5 +17,19 @@ list3.add(75);
 list3.removeFirst();
 
        System.out.println(list3.reversed() );
+
+
+       System.out.println( list3.peek());
+//       adds teh elemtn at last
+       System.out.println(list3.offer(9));
+       System.out.println(list3);
+
+//       retrieves and remove the first elemtn fro mthe linnkedlsit
+       System.out.println(list3.poll());
+       System.out.println(list3);
+
+//       Pops an element from the stack represented by this list. In other words, removes and returns the first element of this list
+       System.out.println(list3.pop());
+       list3.addLast(34);
     }
 }
