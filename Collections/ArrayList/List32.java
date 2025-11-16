8dbd6938f3721dd9809de2bdebf4c4373e2d4fa7
Yash Patil <patilyashyp12@gmail.com>
2025-11-16 21:36:55 +0530

    Add List32 example for ArrayList usage
    Introduces List32.java, demonstrating basic instantiation and printing of an empty ArrayList of Integers.

---
+++ Collections/ArrayList/List32.java
@@ -0,0 +1,16 @@
+package Collections.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List32 {
+
+   public static void main(String[] args) {
+
+       List<Integer> lsit34 = new ArrayList<Integer>();
+
+       System.out.println(lsit34);
+
+
+    }
+}
