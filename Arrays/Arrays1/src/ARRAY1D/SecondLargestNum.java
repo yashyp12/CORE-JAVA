69665b3866756273c1d30b458ae27e8eceb98404
Yash Patil <patilyashyp12@gmail.com>
2025-11-21 21:47:27 +0530

    Handle edge cases in second largest number finder
    Improved logic to correctly handle arrays with less than two elements and arrays with all identical elements. Added checks to ensure the second largest is distinct and updated output messages for these cases.

--- Arrays/Arrays1/src/ARRAY1D/SecondLargestNum.java
+++ Arrays/Arrays1/src/ARRAY1D/SecondLargestNum.java
@@ -5,21 +5,37 @@ public class SecondLargestNum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int []arr = {1,3,4,7,6,6,5};
+		int []arr = {5,5,5};
 		
 		int largest = Integer.MIN_VALUE;
 		int SecondLargest = Integer.MIN_VALUE;
+		 
+		if(arr.length<2) {
+			System.out.println("no second largest");
+			largest = Integer.MIN_VALUE;
+			SecondLargest = Integer.MIN_VALUE;
+			return;
+		}
 		
 		for(int i =0;i<arr.length;i++) {
+			
 			if(arr[i]>largest) {
 				SecondLargest = largest;
 				largest = arr[i];
-			}else if(arr[i]>SecondLargest) {
+		 
+			}else if(arr[i]<largest && arr[i]>SecondLargest) {
 				SecondLargest = arr[i];
+			}else if(arr[i]==largest){
+				continue;
 			}
+			
 		}
 		
-		System.out.println("largest is " + largest + "second Largest is " + SecondLargest);
+		if(SecondLargest == Integer.MIN_VALUE) {
+			System.out.println("no second distinct elemnt found");
+		}
+ 		 
+		System.out.println("largest is " + largest + " second Largest is " + SecondLargest);
 	}
 
 }
