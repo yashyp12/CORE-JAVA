e086ab638e4a7d79bebc7f8fded508c3f82c010c
Yash Patil <patilyashyp12@gmail.com>
2025-11-27 20:25:56 +0530

    Comment out multiple catch block examples
    Commented out two example blocks in MultipleCatchBlock1.java to disable their execution. This change may be for demonstration or refactoring purposes.

--- Exceptions_Generics/ExceptionHandlingExamp6/MultipleCatchBlock1.java
+++ Exceptions_Generics/ExceptionHandlingExamp6/MultipleCatchBlock1.java
@@ -4,21 +4,49 @@ public class MultipleCatchBlock1 {
 
     static void main(String[] args) {
 //if teh error occuresisnnde the try inenr try then it checks first inenr the ncheck for the outer catch blocks
-        try{
-            System.out.println("outer try");
+//        try{
+//            System.out.println("outer try");
+//
+//            try{
+//                System.out.println("inner trY");
+//                System.out.println(args[0]);
+//            }catch(Exception e){
+//                System.out.println("inner catch");
+//                System.out.println(10/0);
+//            }
+//        }
+//        catch (ArithmeticException e){
+//            System.out.println("outer catch 1");
+//        }catch (RuntimeException e){
+//            System.out.println("outer catch 2");
+//        }
 
-            try{
-                System.out.println("inner trY");
-                System.out.println(args[0]);
-            }catch(Exception e){
-                System.out.println("inner catch");
-                System.out.println(10/0);
-            }
-        }
-        catch (ArithmeticException e){
-            System.out.println("outer catch 1");
-        }catch (RuntimeException e){
-            System.out.println("catch 2");
-        }
+
+//        example 2
+//        try{
+//            System.out.println("outer try");
+//
+//            try{
+//                System.out.println("inner try");
+//                System.out.println(args[0]);
+//            }catch (Exception e){
+//                System.out.println("inner catch");
+//                String []arr = new String[-1];
+//            }
+//        }
+//        catch (ArithmeticException ae){
+//            System.out.println("outer catch 1");
+//        }catch(RuntimeException e){
+//            System.out.println("outer catch 2");
+//        }
+
+
+        
+
+
+
+
+
+
     }
 }
