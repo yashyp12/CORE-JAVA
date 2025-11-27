6e68a3a34aa443ce2fbc005ae383d62d7cd0af8d
Yash Patil <patilyashyp12@gmail.com>
2025-11-27 20:04:54 +0530

    Add Student class with exception handling example
    Introduces the Student class and Example1 to demonstrate exception handling during object creation and percentage calculation. Includes logic for handling errors when invalid marks or totals are provided.

---
+++ Exceptions_Generics/ExceptionHandlingExamp6/Student.java
@@ -0,0 +1,41 @@
+package Exceptions_Generics.ExceptionHandlingExamp6;
+
+public class Student {
+
+    String name;
+    double per;
+
+
+    Student(String name,int marks,int total) throws Exception
+    {
+        this.name = name;
+        this.per = calcPercentage(marks,total);
+    }
+
+    public double calcPercentage(int marks,int total) throws Exception{
+        return (marks/total)*100;
+    }
+
+    public String toString(){
+        return "Name :" + name + " , Percentage :" + per + " %";
+
+    }
+}
+
+
+class Example1{
+
+
+    static void main(String[] args) {
+
+        Student obj = null;
+
+        try{
+            obj = new Student("Yash",470,50);
+        }catch (Exception e){
+            System.out.println("something went wrong exception handled");
+
+        }
+        System.out.println(obj);
+    }
+}
