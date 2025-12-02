2edaf721ea2687c3e060932f53c50493b60e9f55
Yash Patil <patilyashyp12@gmail.com>
2025-12-02 23:15:09 +0530

    Add package declaration and commented nested try-catch
    Added 'package Exceptions_Generics.Finally;' to Example2.java and included a commented-out nested try-catch-finally block for demonstration purposes. No changes to runtime logic; main structure remains the same.

--- Exceptions_Generics/Finally/Example2.java
+++ Exceptions_Generics/Finally/Example2.java
@@ -1,19 +1,44 @@
+package Exceptions_Generics.Finally;
+
 public class Example2 {
     public static void main(String[] args) {
-        
+
         try {
             System.out.println("try");
             System.out.println(10/0);
+
         } catch (ArithmeticException e ) {
             System.out.println("catch block 1");
-            String str = null;
-            str.length();
+        String str = null;
+        str.length();
         }
         catch (Exception e){
             System.out.println("catch block 2");
         } finally{
             System.out.println("finally block ");
         }
+
+
+//
+//        try {
+//            System.out.println("try");
+//            System.out.println(10 / 0);
+//
+//        } catch (ArithmeticException e) {
+//            System.out.println("outer catch block 1");
+//
+//            try{
+//                System.out.println("inner try block");
+//                System.out.println(args[0]);
+//            }
+//            catch(NullPointerException npe){
+//                System.out.println("Inner catch ");
+//            }
+//        }   finally {
+//            System.out.println("finally block ");
+//        }
+
+
         System.out.println("main ends");
     }
 }
