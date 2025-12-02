a4f72a07148516ada951eb746608fa01f0beb688
Yash Patil <patilyashyp12@gmail.com>
2025-12-02 23:44:43 +0530

    Refactor nested try-catch in Example2 and add ThrowsMethod
    Improved the nested try-catch-finally structure in Example2.java for better exception handling and clarity. Added ThrowsMethod.java to demonstrate throwing and catching ArithmeticException in a custom method.

--- Exceptions_Generics/Finally/Example2.java
+++ Exceptions_Generics/Finally/Example2.java
@@ -60,22 +60,73 @@ public class Example2 {
 //
 //
 //
+//       try{
+//           System.out.println("outer try block");
+//
+//           try{
+//               System.out.println("inner try block");
+//               System.out.println(args[0]);
+//           }
+//           catch(ArrayIndexOutOfBoundsException are){
+//               System.out.println("inner catch");
+//               String str =null;
+//               str.length();
+//           }
+//       }catch(ArithmeticException e ){
+//           System.out.println("outer catch block 1");
+//       }catch (NullPointerException npe){
+//           System.out.println("outer catch block 2");
+//       }finally {
+//           System.out.println("finally");
+//       }
+
+
+
+//
+//        try{
+//            System.out.println("outer try block");
+//
+//            try{
+//                System.out.println("inner try block");
+//                System.out.println(args[0]);
+//            }catch (ArrayIndexOutOfBoundsException are){
+//                System.out.println("inner catch");
+//                String str = null;
+//                str.length();
+//            }finally {
+//                System.out.println("inner finally");
+//            }
+//        }catch (ArithmeticException ae){
+//            System.out.println("outer catch 1");
+//        }catch (NullPointerException npe){
+//            System.out.println("outer catch 2 ");
+//        }finally {
+//            System.out.println("outer finally block");
+//        }
+
+
+
+//
         try{
-            System.out.println("outer try bloclk");
-            System.out.println(10/0);
-        }catch (ArithmeticException e){
-            System.out.println("outer catch block 1");
+            System.out.println("outer try block");
+
             try{
-                System.out.println("inner try block ");
+                System.out.println("inner try block");
                 System.out.println(args[0]);
-            }catch (NullPointerException npe){
+            }catch (ArrayIndexOutOfBoundsException ar){
                 System.out.println("inner catch");
+                String str = null;
+                str.length();
+            }finally {
+                System.out.println("inner finally");
             }
-        }catch (ArrayIndexOutOfBoundsException aife){
+        }catch (ArithmeticException ar){
+            System.out.println("outer catch block 1");
+        }catch (NullPointerException npe){
             System.out.println("outer catch 2");
         }
-        finally{
-            System.out.println("finally block");
+        finally {
+            System.out.println("outer finally block");
         }
 
         System.out.println("main ends");
---
+++ Exceptions_Generics/Throwable/ThrowsMethod.java
@@ -0,0 +1,35 @@
+package Exceptions_Generics.Throwable;
+
+public class ThrowsMethod {
+
+    static void main() {
+
+//        try{
+//            int[]arr = {10,20,30,40};
+//            arr[4] = 100000;
+//        }catch (ArrayIndexOutOfBoundsException ar){
+//            System.out.println(ar.getMessage());
+//        }
+        m1(2,0);
+    }
+
+    static void m1(int num1 ,int num2)
+    {
+        System.out.println("m1 starts ");
+   try{
+        System.out.println("try");
+        if(num2 == 0){
+            throw new ArithmeticException("myMessae");
+        }
+       System.out.println(num1/num2);
+    }
+    catch (ArithmeticException a){
+        System.out.println("Catch");
+        System.out.println(a.getClass().getName() + ":" + a.getMessage());
+    }
+//java.lang.ArithmeticException:myMessae
+
+    }
+
+
+}
