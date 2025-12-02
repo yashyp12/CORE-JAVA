3ffbf2d5671280b5ec2ac3fd8ebb51021c69443a
Yash Patil <patilyashyp12@gmail.com>
2025-12-02 23:21:56 +0530

    Refactor Example2 to demonstrate nested try-catch-finally
    Commented out the previous try-catch-finally example and added a new implementation showing nested try-catch blocks with finally. This provides a clearer demonstration of exception handling flow in Java.

--- Exceptions_Generics/Finally/Example2.java
+++ Exceptions_Generics/Finally/Example2.java
@@ -2,21 +2,21 @@ package Exceptions_Generics.Finally;
 
 public class Example2 {
     public static void main(String[] args) {
-
-        try {
-            System.out.println("try");
-            System.out.println(10/0);
-
-        } catch (ArithmeticException e ) {
-            System.out.println("catch block 1");
-        String str = null;
-        str.length();
-        }
-        catch (Exception e){
-            System.out.println("catch block 2");
-        } finally{
-            System.out.println("finally block ");
-        }
+//
+//        try {
+//            System.out.println("try");
+//            System.out.println(10/0);
+//
+//        } catch (ArithmeticException e ) {
+//            System.out.println("catch block 1");
+//        String str = null;
+//        str.length();
+//        }
+//        catch (Exception e){
+//            System.out.println("catch block 2");
+//        } finally{
+//            System.out.println("finally block ");
+//        }
 
 
 //
@@ -38,6 +38,45 @@ public class Example2 {
 //            System.out.println("finally block ");
 //        }
 
+
+//        try{
+//            System.out.println("outer try bloclk");
+//            System.out.println(10/0);
+//        }catch (ArithmeticException e){
+//            System.out.println("outer catch block 1");
+//            try{
+//                System.out.println("inner try block ");
+//                System.out.println(args[0]);
+//            }catch (NullPointerException npe){
+//                System.out.println("inner catch");
+//            }
+//        }catch (ArrayIndexOutOfBoundsException aife){
+//            System.out.println("outer catch 2");
+//        }
+//        finally{
+//            System.out.println("finally block");
+//        }
+//
+//
+//
+//
+        try{
+            System.out.println("outer try bloclk");
+            System.out.println(10/0);
+        }catch (ArithmeticException e){
+            System.out.println("outer catch block 1");
+            try{
+                System.out.println("inner try block ");
+                System.out.println(args[0]);
+            }catch (NullPointerException npe){
+                System.out.println("inner catch");
+            }
+        }catch (ArrayIndexOutOfBoundsException aife){
+            System.out.println("outer catch 2");
+        }
+        finally{
+            System.out.println("finally block");
+        }
 
         System.out.println("main ends");
     }
