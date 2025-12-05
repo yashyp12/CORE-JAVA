7d3ad472f082e35e232fa8fb11cf4ed19c4597aa
Yash Patil <patilyashyp12@gmail.com>
2025-12-05 21:40:53 +0530

    Add custom exception classes and remove examp.java
    Introduced custom exception handling for string operations in MyStringException.java and added a BankDriver stub for future bank app logic. Removed examp.java and its related bank/exception logic to refactor and organize exception handling into dedicated files.

---
+++ Exceptions_Generics/CustomException/BankApp/BankDriver.java
@@ -0,0 +1,7 @@
+package BankApp;
+
+public class BankDriver {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Exceptions_Generics/CustomException/MyString/MyStringException.java
@@ -0,0 +1,57 @@
+package Exceptions_Generics.CustomException.MyString;
+
+class MyStringIndexOtOfBoundsException extends IndexOutOfBoundsException{
+    MyStringIndexOtOfBoundsException(String message){
+        super(message);
+    }
+}
+
+class MyNullPointerException extends RuntimeException{
+    MyNullPointerException(String message){
+        super(message);
+    }
+}
+
+class MyString{
+    char []arr ;
+
+    MyString(String str){
+        arr = new char[str.length()];
+        for(int i =0; i<str.length();i++){
+            arr[i] = str.charAt(i);
+        }
+    }
+    @Override
+    public String toString(){
+        return new String(arr);
+    }
+
+    public char charAt(int index){
+        if(arr == null){
+            throw new MyNullPointerException("ref is null");
+        }
+        if(index<0 || index>=arr.length)
+            throw new MyStringIndexOtOfBoundsException("INDEX OUT OF BOUND ");
+        return arr[index];
+    }
+}
+
+
+public class MyStringException {
+
+    static void main(String[] args) {
+         String obj = new String("hi");
+    MyString str = new MyString(obj);
+        System.out.println(str);
+        System.out.println(str.charAt(1));
+        str = null;
+        if(str == null){
+            throw new MyNullPointerException("REF IS NULL");
+        }
+        System.out.println(str.charAt(1));
+
+
+    }
+
+
+}
