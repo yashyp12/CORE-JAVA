ba715458407da6d47ae814d12f8971383dc24c64
Yash Patil <patilyashyp12@gmail.com>
2025-12-10 08:28:34 +0530

    ADDDDED MISSING NUMBER

--- Arrays/PractArray31/MissingNum.java
+++ Arrays/PractArray31/MissingNum.java
@@ -3,11 +3,11 @@ package PractArray31;
 import java.util.Arrays;
 
 //find the missing num from an array
-// [1,2,3,5 ]
+// [1,2,3,5 ] - FORMULA WORKS ONLY FOR THE 1 TO NATURAL NUMBERS
 public class MissingNum {
 
    public static void main(String[] args) {
-       int []arr = {12,13,15,16};
+       int []arr = {1,2,3,5};
        int n = arr.length+1;
        int sum = 0;
        int expectedSum = n *(n+1)/2;
