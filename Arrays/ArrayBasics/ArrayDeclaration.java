b57f805e078a6497c087bd8f68fc0335eb4d6b46
Yash Patil <patilyashyp12@gmail.com>
2025-12-10 22:16:01 +0530

    Add examples of Java array declarations
    Introduces ArrayDeclaration.java demonstrating various ways to declare arrays in Java, including 1D, 2D, and jagged arrays. Also prints a sample jagged array using Arrays.deepToString.

---
+++ Arrays/ArrayBasics/ArrayDeclaration.java
@@ -0,0 +1,26 @@
+package ArrayBasics;
+
+import java.util.Arrays;
+
+public class ArrayDeclaration {
+
+    public static void main(String[] args) {
+        int[] a; // 1 -d arr
+        int b[]; // // 1-d
+        int[][] c; // 2-d
+        int[] d[]; // same as int [][]d
+        int[][] e; // 2d array
+        int[] f, g; // both f and g are int []
+        int h[], i; // h is int[] , i is varaible
+        int[] j[], k; // j is int [][] , k is int[]
+        int[][] l[], m, n; // l is [][][] and m and n are int[][]
+        int[][] aa[], bb[], cc; // aa and bb are int[][][] cc is int[][]
+
+        int[][] jaggedArr = {
+                { 1, 2 }, { 3, 4, 5 }, { 6, 7, 8, 9 }
+        };
+
+        System.out.println(Arrays.deepToString(jaggedArr));
+
+    }
+}
