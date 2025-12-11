24f3af2e6381f2aad169a4887b329653e5323405
Yash Patil <patilyashyp12@gmail.com>
2025-12-11 21:54:05 +0530

    Refactor array examples and add jagged array demo
    Commented out previous array declarations in ArrayDeclaration.java and added a new example using a 2D byte array. Introduced jaggedarr.java to demonstrate jagged and multidimensional array initialization. Updated several compiled class files, likely reflecting recent changes in source code.

---
+++ Arrays/ArrayBasics/jaggedarr.java
@@ -0,0 +1,27 @@
+package ArrayBasics;
+
+import java.util.Arrays;
+
+public class jaggedarr {
+    public static void main(String[] args) {
+        // int[][]a = new int[2][];
+
+        // a[0] = new int[2];
+        // a[1] = new int[4];
+
+        // System.out.println(Arrays.deepToString(a) );
+
+        int[][][] a = new int[3][][];
+        a[0] = new int[2][3];
+        a[1] = new int[1][5];
+        a[2] = new int[2][];
+        a[3] = new int[3][];
+        a[4] = new int[3][];
+
+        // a[0][0][0] = 1;
+        // a[0][0][1] = 2;
+        // a[0][0][2] = 3;
+
+        System.out.println(Arrays.deepToString(a));
+    }
+}
