70d1c8d127cc39a6fd3f26762c4a707f9797eece
Yash Patil <patilyashyp12@gmail.com>
2025-12-13 21:56:11 +0530

    feat: Add various array manipulation programs
    - Implemented ArrayList examples demonstrating dynamic array behavior.
    - Created programs for summing elements in arrays and finding missing elements.
    - Added functionality for multi-dimensional arrays and their summation.
    - Developed methods for reversing arrays and swapping elements.
    - Included examples for finding the second largest element and calculating factorials.
    - Enhanced documentation on arrays and ArrayLists in markdown format.
    - Added input handling for user-defined arrays and their properties.

---
+++ Arrays/ARRAY-BASICS/TraversingArr.java
@@ -0,0 +1,106 @@
+class TraversingArr {
+
+    public static void main(String[] args) {
+
+        // forwardd traversing
+        // using for whil do whil for each
+
+        int[] arr = { 10, 20, 20, 40, 50 };
+        // for(int i = 0; i<arr.length;i++){
+        // System.out.println(arr[i]);
+        // }
+
+        // int i = 0;
+        // while (i < arr.length) {
+        // System.out.println(arr[i]);
+        // i++;
+        // }
+
+        // int i = 0;
+        // do {
+        // System.out.println(arr[i]);
+        // i++;
+        // } while (i < arr.length);
+
+        //
+
+        // for (int i : arr) {
+        // System.out.println(i);
+        // }
+
+        // 2d arr
+        // int [][]arr2 = { {10,20,30,40},{50,60,70,80,90,100}};
+
+        // for(int i =0;i<arr2.length;i++){
+        // for(int j = 0;j<arr2[i].length;j++){
+        // System.out.println(arr2[i][j]);
+        // }
+        // }
+
+        // backward traversing
+
+        // for(int i = arr.length-1;i>=0;i--){
+        // System.out.println(arr[i]);
+        // }
+
+        //
+        // int i = arr.length - 1;
+        // while(i>=0){
+        // System.out.println(arr[i]);
+        // i--;
+        // }
+
+        // do {
+        // System.out.println(arr[i]);
+        // i--;
+        // } while (i >= 0);
+
+        // 2d arr
+        int[][] a = { { 10, 20, 30, 40 }, { 50, 60, 70, 80, 90, 100 } };
+        // for(int i=0;i<a.length;i++){
+        // for(int j=0;j<a[i].length;j++){
+        // System.out.println(a[i][j]);
+        // }
+        // }
+
+        // do while
+        // int i = 0;
+        // do {
+        // int j =0;
+        // do {
+        // System.out.println(a[i][j]);
+        // j++;
+        // } while (j<a[i].length);
+        // System.out.println();
+
+        // i++;
+        // } while (i<a.length);
+
+        // while
+
+        // int i = 0;
+        // while (i < a.length) {
+
+        //     int j = 0;
+        //     while (j < a[i].length) {
+        //         System.out.println(a[i][j]);
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+        // reverse traversing
+        // for (int i = a.length - 1; i >= 0; i--) {
+        // for (int j = a[i].length - 1; j >= 0; j--) {
+        // System.out.println(a[i][j]);
+        // }
+        // }
+
+
+
+        
+
+
+    }
+}
