65541b357ef53cdef9cbf43aec5699a598a7deb3
Yash Patil <patilyashyp12@gmail.com>
2025-12-15 23:59:12 +0530

    Add comment for while loop in TraversingArr.java
    Added a comment indicating the use of a while loop for traversing the array to improve code readability.

--- Arrays/ARRAY-BASICS/TraversingArr.java
+++ Arrays/ARRAY-BASICS/TraversingArr.java
@@ -100,6 +100,7 @@ class TraversingArr {
 
         int[][] a = { { 10, 20, 30, 40 }, { 50, 60, 70, 80, 90, 100 } };
 
+        // using while loop
         int i = a.length - 1;
         while (i >= 0) {
 
