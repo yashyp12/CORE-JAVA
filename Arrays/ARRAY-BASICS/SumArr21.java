1cc9d70ab6db73f85812e2e96f42b2f05c3cec3d
Yash Patil <patilyashyp12@gmail.com>
2025-12-16 15:00:49 +0530

    feat: Implement SumArr21 class with array sum logic

---
+++ Arrays/ARRAY-BASICS/SumArr21.java
@@ -0,0 +1,31 @@
+public class SumArr21 {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 2, 4, 3, 6, 5 };
+
+        int sum = 0;
+        // for (int number : arr) {
+        // sum += number;
+        // }
+
+        // for(int i =0;i <arr.length;i++){
+        // sum += arr[i];
+        // }
+
+        // int i = 0;
+        // while(i<arr.length){
+        // sum+=arr[i];
+        // i++;
+        // }
+        // System.out.println(sum);
+
+        // int i = 0;
+        // do {
+        // sum += arr[i];
+        // i++;
+        // } while (i < arr.length);
+        // System.out.println(sum);
+
+    }
+}
