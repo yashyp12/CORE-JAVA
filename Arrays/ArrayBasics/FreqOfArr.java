9164375bf17c9d7000c5aa6e65627ab49de01409
Yash Patil <patilyashyp12@gmail.com>
2025-12-19 22:30:43 +0530

    Add array frequency and matrix sum examples
    Added FreqOfArr.java with commented code for finding frequency of an element in an array. Updated TrichyArr.java to include a 2D matrix sum example and import Arrays, while commenting out previous examples.

---
+++ Arrays/ArrayBasics/FreqOfArr.java
@@ -0,0 +1,32 @@
+public class FreqOfArr {
+    public static void main(String[] args) {
+        
+
+        // int [] arr = {10,20,10,30,10};
+        // int key = 10;
+        // int count =0 ;
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     if(arr[i] == key){
+        //         count++;
+        //     }
+        // }
+
+        // System.out.println("freq of the elemtn is " + count);
+
+        // 
+
+
+
+        
+
+
+
+
+
+    }
+
+
+ 
+}
+
--- Arrays/ArrayBasics/TrichyArr.java
+++ Arrays/ArrayBasics/TrichyArr.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class TrichyArr {
 
@@ -6,20 +7,31 @@ public class TrichyArr {
         // 1
         // int[] d = { 1, 2, 3, 4 };
         // for (int i = 1; i < d.length; i++) {
-        //     d[i] = d[i] + d[i - 1];
+        // d[i] = d[i] + d[i - 1];
         // }
         // System.out.println(Arrays.toString(d));
 
         // 2 max el
 
-        int max = Integer.MIN_VALUE;
-        int []arr = { 1,2,3,4,5};
-        for(int i =0;i<arr.length;i++)
-        {
-            if(arr[i]>max) max = arr[i];
+        // int max = Integer.MIN_VALUE;
+        // int []arr = { 1,2,3,4,5};
+        // for(int i =0;i<arr.length;i++)
+        // {
+        // if(arr[i]>max) max = arr[i];
+        // }
+
+        // System.out.println(max);
+
+        int[][] a = { { 2, 3 }, { 4, 5 } };
+        int[][] b = { { 4, 5 }, { 6, 7 } };
+        int[][] sum = new int[2][2];
+
+        for (int i = 0; i < sum.length; i++) {
+            for (int j = 0; j < sum.length; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+            }
         }
 
-        System.out.println(max);
-        
+        System.out.println(Arrays.deepToString(sum));
     }
 }
