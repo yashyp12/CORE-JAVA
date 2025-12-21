3491c54b3653fa1f707b563f3c4e12c0a27c9258
Yash Patil <patilyashyp12@gmail.com>
2025-12-21 23:42:39 +0530

    Add file I/O example and documentation
    Added IOFILEHANDLING.MD to explain internal file I/O processes. Created data.txt with sample content. Updated FileHandling.java to include commented examples of FileInputStream and FileOutputStream usage for binary file operations.

--- FileHandling/src/FileHandling.java
+++ FileHandling/src/FileHandling.java
@@ -1,8 +1,32 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FileHandling {
 
-   public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-    }
+   public static void main(String[] args) throws IOException {
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println();
+
+      // file output steam is class writ th binary data into the file
+      // FileOutputStream fos = new FileOutputStream("data.txt");
+
+      // FileInputStream fis = new FileInputStream("data.txt");
+
+      // int byteData;
+
+      // while ((byteData = fis.read()) != -1) {
+
+      //    System.out.println((char) byteData);
+      // }
+
+      // fis.close();
+
+      // FileOutputStream fos = new FileOutputStream("data.txt");
+      // fos.write(65);
+      
+      
+
+      
+   }
 }
