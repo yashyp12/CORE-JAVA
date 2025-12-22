a5f25dee7674c97b47a1b999572c117125c39651
Yash Patil <patilyashyp12@gmail.com>
2025-12-22 20:15:35 +0530

    feat: Implement duplicate element detection in duplicateElement class

--- Arrays/ARRAY2.0/PROGRAMS 1D/duplicateElement.java
+++ Arrays/ARRAY2.0/PROGRAMS 1D/duplicateElement.java
@@ -1,40 +1,33 @@
-
-class distinctElement {
+public class duplicateElement {
 
     public static void main(String[] args) {
 
-        int[] arr = { 20, 20, 10, 12, 13, 20, 12 };
+        int[] arr = { 12, 21, 25, 12,21 };
 
-        int[] count = frequArr(arr);
-
-        // counting the distinct elements from it
+        int[] count = duplicate(arr);
+        // duplicates
         for (int i = 0; i < count.length; i++) {
-            if (count[i] == 1) {
+            if (count[i] > 1) {
                 System.out.println(arr[i]);
             }
         }
-
     }
 
-    static int[] frequArr(int[] arr) {
-
+    public static int[] duplicate(int[] arr) {
         int[] count = new int[arr.length];
+
         for (int i = 0; i < arr.length; i++) {
-
-            if (count[i] == -1)
-                continue;
-
             int cnt = 1;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] == arr[j]) {
                     cnt++;
                     count[j] = -1;
                 }
+
             }
 
             count[i] = cnt;
         }
-
         return count;
     }
 }
