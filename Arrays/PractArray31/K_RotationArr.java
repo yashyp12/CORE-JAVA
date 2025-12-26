e59f36cf518a145f7a780a76dbc30e92b8d32205
Yash Patil <patilyashyp12@gmail.com>
2025-12-27 00:45:15 +0530

    Add right rotation method for array by k positions
    Introduced kReverseRight to rotate the array right by k positions, updated main to use this method, and renamed the original rotation method to kReverseLeft for clarity. The array size in main was also updated for demonstration.

--- Arrays/PractArray31/K_RotationArr.java
+++ Arrays/PractArray31/K_RotationArr.java
@@ -7,15 +7,17 @@ public class K_RotationArr {
 
     static void main(String[] args) {
 
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
         System.out.println("Enter the value of K ");
         int k = new Scanner(System.in).nextInt();
         System.out.println("array before reverse " + Arrays.toString(arr));
-        kReverse(arr, k);
-        System.out.println("reversed array by k times " + Arrays.toString(arr));
+//        kReverseLeft(arr, k);
+        kReverseRight(arr, k);
+//        System.out.println("reversed array by left shifting k times " + Arrays.toString(arr));
+        System.out.println("reversed array by right shifting k times " + Arrays.toString(arr));
     }
 
-    static void kReverse(int[] arr, int k) {
+    static void kReverseLeft(int[] arr, int k) {
         int n = arr.length;
         k = k % n; // handles the larger k
         //step 1 -> reverse the K elemetn first from the array
@@ -25,6 +27,18 @@ public class K_RotationArr {
         reverse(arr, k, n - 1); // step 2
         reverse(arr, 0, n - 1); // step 3
     }
+
+    static void kReverseRight(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n; // handles the larger k
+        //step 1 -> reverse the last k elements
+        //step 2 -> reverse the remaining first n-k elements means , (n-k-1) elements from the array
+        //step 3 -> reverse the whole arr elemetn from the array
+        reverse(arr, k - 1, n - 1);
+        reverse(arr, 0, n - k - 1);    // n-k-1 - 5 - 3- 1 = 1 -> 0,1
+        reverse(arr, 0, n - 1);
+    }
+
 
     static void reverse(int[] arr, int start, int end) {
         while (start < end) {
