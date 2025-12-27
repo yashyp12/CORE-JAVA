f26cec5224492bf5872860c4b9da768bccad8c16
Yash Patil <patilyashyp12@gmail.com>
2025-12-27 20:38:55 +0530

    Add solution for two sum problem in sum34.java
    Introduces sum34.java with a basic implementation to find indices of two consecutive numbers in an array that add up to a target value. Includes sample input and prints the result.

---
+++ Arrays/PractArray31/LeetCode/sum34.java
@@ -0,0 +1,30 @@
+//package PractArray31.LeetCode;
+//
+//import java.util.Arrays;
+//
+//public class sum34 {
+//    static void main(String[] args) {
+////        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+////You may assume that each input would have exactly one solution, and you may not use the same element twice.
+////You can return the answer in any order.
+////
+////Example 1:
+////Input: nums = [2,7,11,15], target = 9
+////Output: [0,1]
+////Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+//
+//        int arr[] = {2,7,11,15};
+//        int target = 6;
+//        int res[]=new int[4];
+//
+//        for(int i =0;i<arr.length;i++){
+//            if(arr[i]+arr[i+1]==target)
+//            {
+//                res[i] = i;
+//                res[i+1] = i+1;
+//            }
+//        }
+//
+//        System.out.println(Arrays.toString(res));
+//    }
+//}
