26a3f4a113b191f0b7a6eb964d98ce6d2f576fc6
Yash Patil <patilyashyp12@gmail.com>
2025-12-30 14:10:27 +0530

    Add IntelliJ project files and array traversal comments
    Added IntelliJ IDEA project configuration files for Arrays3 module. Updated DeclarationArr.java with commented examples of different 1D array traversal methods for reference.

--- Arrays/Arrays3/DeclarationArr.java
+++ Arrays/Arrays3/DeclarationArr.java
@@ -6,6 +6,26 @@ public class DeclarationArr {
 
     static void main(String[] args) {
         int[]arr = {1,2,3,4,5};
-        System.out.println(Arrays.toString(arr));
-    }
+
+//        forward traversing for the 1d array
+//        for(int i =0;i<arr.length;i++)
+//        {
+//            System.out.println(arr[i]);
+//        }
+
+//        for(int i:arr){
+//            System.out.print(i);
+//        }
+
+//        int i =0;
+//        do{
+//            System.out.println(arr[i]);
+//            i++;
+//        }while(i<=arr.length-1);
+
+
+//    backward traversing of array 1d
+
+
+     }
 }
