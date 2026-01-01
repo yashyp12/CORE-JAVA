d65d159ecf0b9dc2c5298d405568bb7f7c9e4dd7
Yash Patil <patilyashyp12@gmail.com>
2026-01-01 22:46:21 +0530

    Add clockwise array rotation method
    Introduced rotateClockwise method in RotateArr.java to support rotating arrays in the clockwise direction. Updated main to demonstrate clockwise rotation and improved rotation logic to handle redundant rotations. Cleaned up Test.java by removing unused code.

--- ArraysPrograms/src/arrays/oned/RotateArr.java
+++ ArraysPrograms/src/arrays/oned/RotateArr.java
@@ -6,11 +6,17 @@ import java.util.Arrays;
 public class RotateArr {
     static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        int k = 2;
-        rotateAntiClockwise(arr, k);
+        int k = 3;
+//        rotateAntiClockwise(arr, k);
+        rotateClockwise(arr,k);
     }
 
     static void rotateAntiClockwise(int[] arr, int rotation) {
+//        k % n removes unnecessary full rotations because rotating n times gives the same array.
+        /*Rotating an array n times brings it back to the same array.
+So anything more than n rotations is useless repetition. */
+
+        rotation = rotation%arr.length;
         for (int i = 1; i <=rotation; i++) {
             int temp = arr[0];
             for (int j = 0; j < arr.length-1; j++) {
@@ -20,4 +26,20 @@ public class RotateArr {
         }
         System.out.println("rotated to clockwise arr " + Arrays.toString(arr));
     }
+
+    static void rotateClockwise(int[]arr,int rotation){
+        rotation = rotation%arr.length;
+
+        for(int i =1;i<=rotation;i++){
+            int temp = arr[arr.length-1];
+            for(int j = arr.length-1;j>0;j--)
+            {
+                arr[j] = arr[j-1];
+            }
+            arr[0] = temp;
+        }
+        System.out.println("rotated to Anticlockwise arr " + Arrays.toString(arr));
+
+    }
+
 }
