93da85e0570b6caafcf9110587f2c3276b5c588b
Yash Patil <patilyashyp12@gmail.com>
2026-01-01 17:57:35 +0530

    Fix 2D array merge logic in MergeArr.java
    Corrected the logic for merging a 2D char array into a 1D array. The previous implementation incorrectly calculated the size and did not merge elements properly. Added code to flatten the 2D array and print the result.

--- ArraysPrograms/src/arrays/twod/MergeArr.java
+++ ArraysPrograms/src/arrays/twod/MergeArr.java
@@ -1,19 +1,32 @@
 package arrays.twod;
 
+import java.util.Arrays;
+
 public class MergeArr {
 
 //    merging the 2d array
 
     static void main(String[] args) {
 
-        char[][]arr ={{'a','b','c'},{'d','e','f'}};
-
+        char[][] arr = {{'a', 'b', 'c'},
+                {'d', 'e', 'f'}};
         int size = 0;
 
-        for(char[]ele:arr){
-            for(int ele1:ele){
-                size+=arr.length;
+        for (char[] ch : arr) {
+            size += ch.length;
+        }
+
+        //make a new aray to tore all elements
+        char[] ch2 = new char[size];
+
+        //now add the elemnts to the new array
+        int i = 0;
+        for (char[] arr1 : arr) {
+            for (char ele : arr1) {
+                ch2[i++] = ele;
             }
         }
+
+        System.out.println(Arrays.toString(ch2));
     }
 }
