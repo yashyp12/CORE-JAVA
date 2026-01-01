9df3ace39bfd1a7ea0a361970407bf14b5fe531e
Yash Patil <patilyashyp12@gmail.com>
2026-01-01 15:43:45 +0530

    Add array merge function to BasicArr
    Introduced the mergeArr method to merge two integer arrays and print the result. Also updated main to demonstrate merging, and improved formatting in productOfArr and charArrExam methods.

--- ArraysPrograms/src/arrays/oned/BasicArr.java
+++ ArraysPrograms/src/arrays/oned/BasicArr.java
@@ -15,6 +15,9 @@ public class BasicArr {
 //        randFuncInit(arr);
 //        charArrExam();
 
+        int[] arr1 = {1, 2, 3, 4}, arr2 = {5, 6, 7};
+        mergeArr(arr1, arr2);
+
 
     }
 
@@ -52,25 +55,40 @@ public class BasicArr {
     static void productOfArr(int[] arr) {
         long product = 1;
         for (int i : arr) {
-             if(i != 0) product *= i;
+            if (i != 0) product *= i;
 
         }
         System.out.println("Product of arr " + product);
     }
 
-    static void charArrExam(){
-        char []arr = new char[26];
+    static void charArrExam() {
+        char[] arr = new char[26];
         System.out.println("before " + Arrays.toString(arr));
-        for(char ch ='A'; ch<='Z'; ch++)
-        {
-            arr[ch-65] = ch;
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            arr[ch - 65] = ch;
         }
         System.out.println("after " + Arrays.toString(arr));
     }
 
-//mergin 2 dif arryas
-    static void mergeArr(int []arr1,int []arr2){
-        // approach -
+    //mergin 2 dif arryas - only unsorted approach
+    static int[] mergeArr(int[] arr1, int[] arr2) {
+        /* approach - assume we have an 2 sorted array
+         - in which create an another one array of arr1+arr2 size
+        - store the max - length of an array
+        - iterate the aray till the max array length
+        */
+
+        int[] c = new int[arr1.length + arr2.length];
+
+        int max = arr1.length < arr2.length ? arr2.length : arr1.length;
+
+        for (int i = 0, ci = 0; i < max; i++) {
+            if (i < arr1.length) c[ci++] = arr1[i];
+            if (i < arr2.length) c[ci++] = arr2[i];
+        }
+
+        System.out.println("update c[] " + Arrays.toString(c));
+        return c;
     }
 
 
