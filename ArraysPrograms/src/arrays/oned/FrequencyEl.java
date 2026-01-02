fb136ce4cc43b718c37da49c85b119f54a1ff7b6
Yash Patil <patilyashyp12@gmail.com>
2026-01-02 23:18:57 +0530

    Add frequency counter and improve max element logic
    Added FrequencyEl.java to count element frequencies in an array. Improved MaxElement.java by refining the logic for finding the second largest element, handling edge cases, and updating test data. Updated corresponding compiled class files.

---
+++ ArraysPrograms/src/arrays/oned/FrequencyEl.java
@@ -0,0 +1,30 @@
+package arrays.oned;
+
+import java.util.Arrays;
+
+public class FrequencyEl {
+
+    static void main(String[] args) {
+       int[]arr = {1,2,1,2,3,4,5,4,4};
+        frequencyEle(arr);
+    }
+    static void frequencyEle(int [] arr){
+        int [] freq = new int[arr.length];
+        for(int i =0;i<arr.length;i++){
+            if(freq[i]==-1) continue;
+            int cnt = 1;
+
+            for(int j = i+1; j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    freq[j] = -1;
+                    cnt++;
+                }
+            }
+            freq[i] = cnt;
+            System.out.println("Freq of element is " + arr[i] + " : " +freq[i] );
+
+        }
+
+    }
+
+}
--- ArraysPrograms/src/arrays/oned/MaxElement.java
+++ ArraysPrograms/src/arrays/oned/MaxElement.java
@@ -1,9 +1,11 @@
 package arrays.oned;
 
+import java.util.Arrays;
+
 public class MaxElement {
 
     static void main(String[] args) {
-        int[] arr = {1, 8,9, 4,8, 5, 6, 7};
+        int[] arr = {12, 12, 12};
 //        largestEl1(arr);
 //        smallestEl1(arr);
         secondLargest(arr);
@@ -12,39 +14,50 @@ public class MaxElement {
     //find the first largest element from the array
     static void largestEl1(int[] arr) {
         int max = Integer.MIN_VALUE;
-       for(int ele:arr){
-           if(ele>max){
-               max=ele;
-           }
-       }
+        for (int ele : arr) {
+            if (ele > max) {
+                max = ele;
+            }
+        }
         System.out.println("Arrays 1st largest element " + max);
     }
 
 
     static void smallestEl1(int[] arr) {
         int min = Integer.MAX_VALUE;
-       for(int ele:arr){
-           if(ele<min){
-               min=ele;
-           }
-       }
+        for (int ele : arr) {
+            if (ele < min) {
+                min = ele;
+            }
+        }
         System.out.println("Arrays 1st smallestt element " + min);
     }
 
-//
+    //
     static void secondLargest(int[] arr) {
+
+        if(arr.length<2) System.out.println("second largest not found ");
+
         int max = Integer.MIN_VALUE;
-        int max2 =Integer.MIN_VALUE;
-        for(int ele:arr){
-            if(ele>max){
+        int max2 = Integer.MIN_VALUE;
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+
+                if(max!=Integer.MIN_VALUE) found = true;
                 max2 = max;
-                max = ele;
-            }
-            else if(ele > max2){
-                max2 = ele;
+                max = arr[i];
+            } else if (arr[i] < max && arr[i] > max2) {
+                max2 = arr[i];
+                found = true;
             }
         }
 
-        System.out.println("Arrays 2ND largest element " + max2);
+        if (found) {
+            System.out.println("second index   found" + max2);
+        } else {
+            System.out.println("not found ");
+        }
     }
 }
