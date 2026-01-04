2f4d1fb76e3a9eafacd89220063573c6d1a1afe3
Yash Patil <patilyashyp12@gmail.com>
2026-01-04 15:25:58 +0530

    Add reverse3 method to ReverseArr.java
    Introduced a new reverse3 method that reverses an array by creating a new array and populating it in reverse order. Updated main to use reverse3 instead of reverse and reverse2.

--- ArraysPrograms/src/arrays/oned/ReverseArr.java
+++ ArraysPrograms/src/arrays/oned/ReverseArr.java
@@ -6,8 +6,9 @@ public class ReverseArr {
     static void main(String[] args) {
 
         int[] arr = {1, 2, 3, 4, 5};
-        reverse(arr);
-        reverse2(arr);
+//        reverse(arr);
+//        reverse2(arr);
+        reverse3(arr);
     }
 
     static void reverse(int[] arr) {
@@ -35,4 +36,19 @@ public class ReverseArr {
         }
         System.out.println("Array reversed " + Arrays.toString(arr));
     }
+
+//
+    static void reverse3(int[]arr){
+        int[] ans = new int[arr.length];
+
+        int i =0;
+
+        while(i<arr.length){
+            ans[i] = arr[arr.length-i-1];
+            i++;
+        }
+
+        System.out.println(Arrays.toString(ans));
+    }
+
 }
