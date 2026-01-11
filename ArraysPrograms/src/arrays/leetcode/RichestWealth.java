f3d5e1c7b30075a4b572c0b23a030207211b1505
Yash Patil <patilyashyp12@gmail.com>
2026-01-11 18:00:52 +0530

    Reformat RichestWealth.java for code style consistency
    Updated spacing and indentation in RichestWealth.java to improve readability and maintain consistent code style. No functional changes were made.

--- ArraysPrograms/src/arrays/leetcode/RichestWealth.java
+++ ArraysPrograms/src/arrays/leetcode/RichestWealth.java
@@ -4,22 +4,22 @@ package arrays.leetcode;
 public class RichestWealth {
 
     static void main(String[] args) {
-       int[][]accounts = {{1,2,3},{3,2,1}};
+        int[][] accounts = {{1, 2, 3}, {3, 2, 1}};
 
         System.out.println(maximumWealth(accounts));
     }
 
 
-      public static int maximumWealth(int[][] accounts) {
+    public static int maximumWealth(int[][] accounts) {
 
         int max = Integer.MIN_VALUE;
 
-        for(int i=0;i<accounts.length;i++){
+        for (int i = 0; i < accounts.length; i++) {
             int sum = 0;
-            for(int j=0;j<accounts[i].length;j++){
-                sum+=accounts[i][j];
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
             }
-            if(sum>max) max = sum;
+            if (sum > max) max = sum;
         }
 
         return max;
