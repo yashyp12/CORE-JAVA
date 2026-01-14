4e428849351ed4caa05645d5d240b5c3f30d9616
Yash Patil <patilyashyp12@gmail.com>
2026-01-14 12:37:23 +0530

    Refactor least frequent element logic in LeastRepEle
    Rewrote the algorithm to use an auxiliary count array to track element frequencies and mark duplicates, improving clarity and correctness in finding the least frequent element.

--- ArraysPrograms/src/arrays/problems/LeastRepEle.java
+++ ArraysPrograms/src/arrays/problems/LeastRepEle.java
@@ -3,32 +3,33 @@ package arrays.problems;
 public class LeastRepEle {
 
 
-//    find the least rep element from teh array means having less frequency element
+    //    find the least rep element from teh array means having less frequency element
     static void main(String[] args) {
-
-        int[]arr = {2,3,66,5,6,2,3,66,5};
-
+        int[] arr = {2, 3, 66, 5, 6, 2, 3, 66, 5};
+        int[] count = new int[arr.length];
         int minfreq = arr.length;
         int minEle = 0;
 
+        for (int i = 0; i < arr.length; i++) {
+            if (count[i] == -1) continue;
 
-        for(int i=0;i<arr.length;i++){
-             int cnt = 1;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                     cnt++;
+            int cnt = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count[j]=-1;
+                    cnt++;
                 }
             }
-           if(cnt<minfreq){
+            count[i] = cnt;
+            if (count[i] < minfreq) {
                 minfreq = cnt;
                 minEle = arr[i];
-           }
+            }
         }
 
 
         System.out.println("Least repeating element in arr is " + minEle);
     }
-
 
 
 }
