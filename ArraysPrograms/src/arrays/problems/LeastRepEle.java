3190e49972c3b31b58e9737ad21f8ed7d7987745
Yash Patil <patilyashyp12@gmail.com>
2026-01-14 12:13:26 +0530

    Add logic to find least repeating element in array
    Implemented functionality in LeastRepEle.java to determine and print the element with the lowest frequency in the array. The main method now initializes an array, computes the least repeating element, and outputs the result.

--- ArraysPrograms/src/arrays/problems/LeastRepEle.java
+++ ArraysPrograms/src/arrays/problems/LeastRepEle.java
@@ -6,13 +6,31 @@ public class LeastRepEle {
 //    find the least rep element from teh array means having less frequency element
     static void main(String[] args) {
 
+        int[]arr = {2,3,66,5,6,2,3,66,5};
+ 
+
+        int minfreq = arr.length;
+        int minEle = 0;
+
+
+        for(int i=0;i<arr.length;i++){
+             int cnt = 1;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    count[j] = -1;
+                    cnt++;
+                }
+            }
+           if(cnt<minfreq){
+                minfreq = cnt;
+                minEle = arr[i];
+           }
+        }
+
+
+        System.out.println("Least repeating element in arr is " + minEle);
     }
 
-    static int leastEle(int []arr){
 
-
-        return -1;
-
-    }
 
 }
