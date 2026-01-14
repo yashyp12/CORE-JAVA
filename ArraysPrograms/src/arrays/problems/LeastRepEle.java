09481d1b79431cff03155f7cbb395ae358a0a562
Yash Patil <patilyashyp12@gmail.com>
2026-01-14 12:02:39 +0530

    Add LeastRepEle problem and update SentinelSearch example
    Added LeastRepEle.java to implement finding the least frequent element in an array. Updated MySentinelEx.java with a complete sentinel search example, including test data and output. Corresponding class files were generated.

---
+++ ArraysPrograms/src/arrays/problems/LeastRepEle.java
@@ -0,0 +1,18 @@
+package arrays.problems;
+
+public class LeastRepEle {
+
+
+//    find the least rep element from teh array means having less frequency element
+    static void main(String[] args) {
+
+    }
+
+    static int leastEle(int []arr){
+
+
+        return -1;
+
+    }
+
+}
--- ArraysPrograms/src/arrays/searching/SentinelSearch/MySentinelEx.java
+++ ArraysPrograms/src/arrays/searching/SentinelSearch/MySentinelEx.java
@@ -3,7 +3,24 @@ package arrays.searching.SentinelSearch;
 public class MySentinelEx {
 
     static void main(String[] args) {
+        int [] arr = {1,3,8,9,2,11,22,40,50,20};
+        int key = 20;
 
-
+        int n = arr.length;
+        int pos = sentinelSearch(arr,n,key);
+        System.out.println(pos);
     }
+    static int sentinelSearch(int[]arr, int n, int key){
+          int last = arr[arr.length-1];
+        arr[arr.length-1] = key;
+        int i =0;
+        while(arr[i]!=key){
+            i++;
+        }
+
+        arr[arr.length-1] = last;
+         if(i<arr.length-1 || last==key) return i;
+         else return -1;
+    }
+
 }
