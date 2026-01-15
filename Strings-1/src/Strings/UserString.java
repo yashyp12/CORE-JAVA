34f09ef58d45102172415b87018fba401bc296a9
Yash Patil <patilyashyp12@gmail.com>
2026-01-15 20:44:14 +0530

    Add string utility methods to UserString class
    Implemented several new methods in UserString: startsWith, endsWith, indexOf (with and without fromIndex), substring, replace, valueOf (int), and contentEquals. Also improved formatting and consistency in existing methods. These additions enhance the functionality of the custom string class to better mimic standard Java String behavior.

--- Strings-1/src/Strings/UserString.java
+++ Strings-1/src/Strings/UserString.java
@@ -39,9 +39,9 @@ public final class UserString {
             throw new MyStringIndexOutOfBoundsException("int array index of bounds");
         }
         this.arr = new char[count];
-         for(int i =0; i<count;i++){
-             this.arr[i] = (char) arr[offset+i];
-         }
+        for (int i = 0; i < count; i++) {
+            this.arr[i] = (char) arr[offset + i];
+        }
     }
 
     public UserString(byte[] arr) {
@@ -105,22 +105,22 @@ public final class UserString {
         return new UserString(newArr);
     }
 
-    public UserString toUpperCase(){
+    public UserString toUpperCase() {
         UserString newstr = new UserString();
         char[] newArr = new char[this.arr.length];
         // copying the orignasl character s to the new char array
         int indx = 0;
-        for(char ele:this.arr){
+        for (char ele : this.arr) {
             newArr[indx++] = ele;
         }
 
         //comversion logic is here
         int indx1 = 0;
-        for(char ele: newArr){
+        for (char ele : newArr) {
             // pending implementation
             //checks whether teh characters is lowercase or not
-            if(ele >= 97 && ele<=122){
-                newArr[indx1]  = (char) (ele - 32);
+            if (ele >= 97 && ele <= 122) {
+                newArr[indx1] = (char) (ele - 32);
             }
             indx1++;
         }
@@ -128,68 +128,215 @@ public final class UserString {
     }
 
 
-    public UserString toLowerCase(){
+    public UserString toLowerCase() {
         UserString newStr = new UserString();
-            char[] newArr = new char[this.arr.length];
+        char[] newArr = new char[this.arr.length];
 
-            int indx = 0;
-            for(char ele : this.arr){
-                newArr[indx++] = ele;
-            }
+        int indx = 0;
+        for (char ele : this.arr) {
+            newArr[indx++] = ele;
+        }
 
-            int indx1 =0;
-            for(char ele:newArr){
-                if(ele>=65 && ele<=90){
-                    newArr[indx1++] = (char) (ele + 32);
-                }
-                indx1++;
+        int indx1 = 0;
+        for (char ele : newArr) {
+            if (ele >= 65 && ele <= 90) {
+                newArr[indx1++] = (char) (ele + 32);
             }
-            return new UserString(newArr);
+            indx1++;
+        }
+        return new UserString(newArr);
     }
 
-    public int codePointAt(int indx){
-        if(indx < 0 || indx > arr.length-1)
+    public int codePointAt(int indx) {
+        if (indx < 0 || indx > arr.length - 1)
             throw new MyStringIndexOutOfBoundsException("invalid index");
 
         //basically this methods returns the asci value
         return this.arr[indx] + 0;
     }
 
-    public int codePointBefore(int indx){
-        if(indx <=0 || indx > arr.length){
+    public int codePointBefore(int indx) {
+        if (indx <= 0 || indx > arr.length) {
             throw new MyStringIndexOutOfBoundsException("invalid index");
         }
         return this.arr[indx - 1] + 0;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return this.arr.length == 0;
     }
 
-    public char[] toCharArray(){
-        char[]newArr = new char[this.arr.length];
+    public char[] toCharArray() {
+        char[] newArr = new char[this.arr.length];
         int indx = 0;
-        for(char ele : this.arr){
+        for (char ele : this.arr) {
             newArr[indx++] = ele;
-         }
+        }
         return newArr;
     }
 
     public boolean equals(Object obj) {
         UserString str = (UserString) obj;
-        if(str.length() != this.length()) return false;
-
-        for(int i =0;i<str.length();i++){
-            if(this.charAt(i) !=str.charAt(i)){
-                return  false;
+        if (str.length() != this.length()) return false;
+        for (int i = 0; i < str.length(); i++) {
+            if (this.charAt(i) != str.charAt(i)) {
+                return false;
             }
         }
         return true;
     }
 
-    public boolean equalsIgnoreCase(UserString comp){
+    public boolean equalsIgnoreCase(UserString comp) {
         return comp.toUpperCase().equals(this.toUpperCase());
     }
+
+    public boolean startsWith(UserString prefix) {
+
+        //condition 1 prefic longer than string
+        if (prefix.arr.length > this.arr.length) {
+            return false;
+        }
+
+//        compare character fro m start
+        for (int i = 0; i < prefix.arr.length; i++) {
+            if (this.arr[i] != prefix.arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean endsWith(UserString suffix) {
+        if (suffix.arr.length > this.arr.length) {
+            return false;
+        }
+
+        int start = this.arr.length - suffix.arr.length;
+        for (int i = 0; i < suffix.arr.length; i++) {
+
+            if (this.arr[start + i] != suffix.arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public int indexOf(char ch){
+
+        for(int i =0;i<this.arr.length;i++){
+            if(this.arr[i] == ch){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int indexOf(char ch,int fromIndex){
+
+        if(fromIndex < 0){
+            fromIndex =0;
+        }
+
+        //CHECK IF THE POINTING INDEX IS OUTSIDE THE ARRAY
+        if(fromIndex >= this.arr.length){
+            return -1;
+        }
+
+        for(int i = fromIndex;i<this.arr.length;i++)
+        {
+            if(this.arr[i] == ch){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public UserString substring(int beginIndex){
+
+        if(beginIndex<0 || beginIndex > this.arr.length)
+        {
+            throw new MyStringIndexOutOfBoundsException("index out of bounds");
+        }
+
+//        calculate new length
+        int newLength = this.arr.length - beginIndex;
+        //create new array
+        char[] newArr = new char[newLength];
+
+        for(int i =0;i<newLength;i++){
+            newArr[i] = this.arr[beginIndex+i];
+        }
+        return new UserString(newArr);
+    }
+
+    public UserString replace(char oldChar, char newChar){
+        char[]newArr = new char[this.arr.length];
+
+        for(int i = 0; i<this.arr.length;i++){
+            if(this.arr[i] == oldChar){
+                newArr[i] = newChar;
+            }else{
+                newArr[i] = this.arr[i];
+            }
+        }
+
+        return new UserString(newArr);
+    }
+
+//    CONVERTS THE primitves to String
+    public static UserString valueOf(int num){
+
+        //handle 0 case
+        if(num == 0){
+            return new UserString("0");
+        }
+
+        boolean isNegative = false;
+        if(num < 0){
+            isNegative = true;
+            num = -num;
+        }
+
+        char[]temp = new char[20];
+        int index = 0;
+
+        while(num>0){
+            int digit = num%10;
+            temp[index++] = (char)('0' + digit);
+            num = num/10;
+        }
+
+        if(isNegative){
+            temp[index++] = '-';
+        }
+
+
+//        reverse array
+        char[]result = new char[index];
+        for(int i =0;i<index;i++){
+            result[i] = temp[index-1-i];
+        }
+
+        return new UserString(result);
+    }
+
+
+    public boolean contentEquals(StringBuffer sb){
+        if(sb.length() != this.arr.length){
+            return false;
+        }
+        for(int i =0;i<this.arr.length;i++){
+            if(this.arr[i] != sb.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+
+
 
 
 
