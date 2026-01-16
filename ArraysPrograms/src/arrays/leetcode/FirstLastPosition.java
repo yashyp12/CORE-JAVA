de02b1657ac048cbfbc26360d339d76ff6fd36f2
Yash Patil <patilyashyp12@gmail.com>
2026-01-17 00:32:14 +0530

    Refactor FirstLastPosition and fix output format
    Refactored the FirstLastPosition.java class to improve code clarity and fix the output format by printing the result using Arrays.toString. Also corrected parameter usage in searchRange and search methods, and removed redundant code.

--- ArraysPrograms/src/arrays/leetcode/FirstLastPosition.java
+++ ArraysPrograms/src/arrays/leetcode/FirstLastPosition.java
@@ -1,9 +1,10 @@
 package arrays.leetcode;
 
+import java.util.Arrays;
+
 //https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 //https://youtu.be/W9QJ8HaRvJQ?si=ns9xlg9y1ZbYGIJ6&t=5063
 public class FirstLastPosition {
-
 
     static void main(String[] args) {
         int[] arr = {5, 7, 7, 7, 7, 8, 8, 10};
@@ -13,50 +14,48 @@ public class FirstLastPosition {
         // if the target == mid then if we want first then we search for left side if last occurence then we search for the right side of the array
 
 
-        System.out.println(searchRange(arr, 7));
+        int []ans = searchRange(arr, 8);
+        System.out.println(Arrays.toString(ans));
     }
 
     public static int[] searchRange(int[] nums, int target) {
-         int[] ans = {-1, -1}; // if not found return -1 both {first occurence , lasst occurecbne}
+        int[] ans = {-1, -1}; // if not found return -1 both {first occurence , lasst occurecbne}
 
+        int start = search(nums, target, true); //now take the left occuence firsty
+        int end = search(nums, target, false);    //take the last occurecn from the right of the array which will be right
 
-        int start = search(arr, 7,true); //now take the left occuence firsty
-        int end = search(arr, 7,false);    //take the last occurecn from the right of the array which will be right
-
+        ans[0] = start;
+        ans[1] = end;
 
         return ans;
     }
 
-    static int search(int[]arr, int target,boolean findStartIndex){
-
-
+    static int search(int[] arr, int target, boolean findStartIndex) {
         int ans = -1;
 
         int start = 0;
-        int end = arr.length-1;
+        int end = arr.length - 1;
 
-        while(start <=end){
-            int mid = start + (end-start)/2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-            if(target<arr[mid]){
-                end = mid-1;
-            }else if(target>arr[mid]){
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
                 start = mid + 1;
-            }else {
+            } else {
                 //means now the potential ans we found which is -> middle == target
                 // now we will check if we want the first occurence then end = mid-1
                 // if we want the last occurenc which will be on the right side then the start  = mid + 1;
                 ans = mid;
-
-                if(findStartIndex){
-                    end = mid -1;
-                }else {
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
                     start = mid + 1;
                 }
             }
-
-            return ans;
         }
+        return ans;
     }
 
 }
