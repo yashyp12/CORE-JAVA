228131686f8b181cf3e0387f03e47ccfc9f11fe1
Yash Patil <patilyashyp12@gmail.com>
2026-01-26 23:00:09 +0530

    Move hashCode wrapper test to HashCodeWrapper.java
    Deleted Test.java from ArrayList and moved its hashCode demonstration code to HashCodeWrapper.java. This consolidates the wrapper class hashCode example into a single file.

--- WrapperClass/HashCodeWrapper.java
+++ WrapperClass/HashCodeWrapper.java
@@ -1,2 +1,14 @@
 public class HashCodeWrapper {
+
+    static void main(String[] args) {
+
+
+        //👉 hashCode() is derived directly from the stored value.
+        // hashcode of the wrapper classes are always its data
+        Integer a = 10;
+        Integer b = 10;
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+    }
 }
