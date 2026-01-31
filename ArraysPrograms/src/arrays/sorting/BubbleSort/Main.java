c4f87a2ca926f8230f2e8e98cc99bf325156d817
Yash Patil <patilyashyp12@gmail.com>
2026-01-31 12:44:27 +0530

    Add BubbleSort Main.java with initial bubble method
    Created Main.java for BubbleSort with a skeleton for the bubble sort algorithm. The method outlines the loop structure but does not yet implement the sorting logic.

---
+++ ArraysPrograms/src/arrays/sorting/BubbleSort/Main.java
@@ -0,0 +1,16 @@
+package BubbleSort;
+//https://youtu.be/F5MZyqRp_IM?si=2skX2Rb2nhFNFhg3
+public class Main {
+
+    static void main(String[] args) {
+
+    }
+    
+    static void bubble(int[]arr){
+//run the steps n -1 times
+        for(int i =0;i<arr.length;i++){
+            //for each step max item will comes at teh last respct index
+
+        }
+    }
+}
