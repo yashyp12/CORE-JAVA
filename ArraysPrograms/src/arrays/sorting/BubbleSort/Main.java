60facc147657559a827cd60431f65c388c48ee74
Yash Patil <patilyashyp12@gmail.com>
2026-01-31 13:57:31 +0530

    Update BubbleSort example array and formatting
    Changed the input array in Main.java to an unsorted sequence for better demonstration of bubble sort. Improved code formatting and spacing for readability.

--- ArraysPrograms/src/arrays/sorting/BubbleSort/Main.java
+++ ArraysPrograms/src/arrays/sorting/BubbleSort/Main.java
@@ -2,11 +2,12 @@ package BubbleSort;
 
 import java.util.Arrays;
 
+//time complexity = O(1)
 //https://youtu.be/F5MZyqRp_IM?si=2skX2Rb2nhFNFhg3
 public class Main {
 
     static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {3, 1, 5, 4, 2};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -30,9 +31,35 @@ public class Main {
 
             //if we not swapped for partiuclar value of i, it means the array is sorted
             //hence stop the program
-            if(!swapped){
+            if (!swapped) {
                 break;
             }
         }
     }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            //find the maximum item in remaining array and swap with correct index
+
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+        }
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[]arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
 }
