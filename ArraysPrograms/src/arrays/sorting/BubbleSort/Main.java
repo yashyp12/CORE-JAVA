579221847b0b4ae65efbfef07c67576d97634802
Yash Patil <patilyashyp12@gmail.com>
2026-01-31 12:52:17 +0530

    Implement bubble sort algorithm in Main.java
    Added a bubble sort implementation to the Main class, including a sample array and output of the sorted result. Improved the bubble method to perform sorting with a swap check and print the sorted array.

--- ArraysPrograms/src/arrays/sorting/BubbleSort/Main.java
+++ ArraysPrograms/src/arrays/sorting/BubbleSort/Main.java
@@ -1,16 +1,38 @@
 package BubbleSort;
+
+import java.util.Arrays;
+
 //https://youtu.be/F5MZyqRp_IM?si=2skX2Rb2nhFNFhg3
 public class Main {
 
     static void main(String[] args) {
-
+        int[] arr = {1, 2, 3, 4, 5};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
     }
-    
-    static void bubble(int[]arr){
-//run the steps n -1 times
-        for(int i =0;i<arr.length;i++){
-            //for each step max item will comes at teh last respct index
 
+    static void bubble(int[] arr) {
+        //if no swap is occured means our array is sorted
+        boolean swapped;
+//run the steps n -1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            //for each step max item will comes at teh last respct index
+            for (int j = 1; j < arr.length - i; j++) {
+                //swap if the item is samller than the previous item
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            //if we not swapped for partiuclar value of i, it means the array is sorted
+            //hence stop the program
+            if(!swapped){
+                break;
+            }
         }
     }
 }
