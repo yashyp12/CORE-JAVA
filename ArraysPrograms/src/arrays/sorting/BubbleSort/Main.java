677074d90745eec0fdaa525dfd35f0710cef4a6d
Yash Patil <patilyashyp12@gmail.com>
2026-02-01 19:30:01 +0530

    Fix insertion sort loop condition in Main.java
    Corrected the inner loop condition in the insertion sort implementation to ensure proper sorting. The loop now correctly iterates while j > 0 instead of j > -0.

--- ArraysPrograms/src/arrays/sorting/BubbleSort/Main.java
+++ ArraysPrograms/src/arrays/sorting/BubbleSort/Main.java
@@ -71,7 +71,7 @@ public class Main {
     static void insertion(int[] arr) {
 
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j > -0; j--) {
+            for (int j = i + 1; j >0; j--) {
                 if (arr[j] < arr[j - 1]) {
                     swap(arr, j, j - 1);
                 } else {
