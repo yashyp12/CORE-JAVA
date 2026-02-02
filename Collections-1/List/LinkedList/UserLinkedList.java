8837611c68fd48d901611f8a0a7bcf1c48334502
Yash Patil <patilyashyp12@gmail.com>
2026-02-02 13:09:24 +0530

    Add new methods and improve UserLinkedList class
    Implemented additional methods in UserLinkedList such as indexOf, get, addAll, remove, clear, and clone. Improved code formatting and added error handling. Updated Main class to demonstrate new functionality.

--- Collections-1/List/LinkedList/UserLinkedList.java
+++ Collections-1/List/LinkedList/UserLinkedList.java
@@ -1,54 +1,54 @@
 package List.LinkedList;
 
-class UserIndexOutOfBoundsException extends RuntimeException{
-    UserIndexOutOfBoundsException(String desc){
+class UserIndexOutOfBoundsException extends RuntimeException {
+    UserIndexOutOfBoundsException(String desc) {
         super(desc);
     }
 }
 
-class UserNoSuchElementException extends RuntimeException{
-    UserNoSuchElementException(){
+class UserNoSuchElementException extends RuntimeException {
+    UserNoSuchElementException() {
         super();
     }
 }
 
-public class UserLinkedList<E>  {
-    private int indx ;
+public class UserLinkedList<E> implements Cloneable {
+    private int indx;
     private Node<E> head;
     private Node<E> tail;
 
-    private class Node<E>{
+    private class Node<E> {
         E ele;
         Node<E> next;
 
-        public Node(E ele){
+        public Node(E ele) {
             this.ele = ele;
         }
     }
 
     @Override
-    public String toString(){
-        if(indx==0) return "[]";
+    public String toString() {
+        if (indx == 0) return "[]";
 
         Node<E> currEle = head;
         String resp = "[";
 
-        while(currEle.next!=null){
-            resp+= currEle.ele+", ";
+        while (currEle.next != null) {
+            resp += currEle.ele + ", ";
             currEle = currEle.next;
         }
 
-        resp+= currEle.ele+"]";
+        resp += currEle.ele + "]";
         return resp;
     }
 
-    public boolean add(E ele){
+    public boolean add(E ele) {
         Node<E> newNode = new Node(ele);
-        if(indx==0){
+        if (indx == 0) {
             head = newNode;
             tail = head;
             indx++;
-        }else{
+        } else {
             tail.next = newNode;
             tail = newNode;
             indx++;
@@ -56,33 +56,33 @@ public class UserLinkedList<E>  {
         return true;
     }
 
-    public void addLast(E ele){
-        if(size() ==0) throw new UserNoSuchElementException();
+    public void addLast(E ele) {
+        if (size() == 0) throw new UserNoSuchElementException();
         add(ele);
     }
 
-    public void addFirst(E ele){
-        if(size()==0) throw new UserNoSuchElementException();
+    public void addFirst(E ele) {
+        if (size() == 0) throw new UserNoSuchElementException();
         Node<E> newNode = new Node<E>(ele);
-        if(indx==0){
+        if (indx == 0) {
             head = tail = newNode;
-        }else{
-            newNode.next =head;
+        } else {
+            newNode.next = head;
             head = newNode;
         }
         indx++;
     }
 
-    public int size(){
+    public int size() {
         return indx;
     }
 
 
-    public E removeLast(){
-        if(size()==0) throw new UserNoSuchElementException();
+    public E removeLast() {
+        if (size() == 0) throw new UserNoSuchElementException();
         E temp = tail.ele;
         Node<E> curr = head;
-        for(int i =0;i<size()-2;i++){
+        for (int i = 0; i < size() - 2; i++) {
             curr = curr.next;
         }
         tail = curr;
@@ -91,8 +91,8 @@ public class UserLinkedList<E>  {
         return temp;
     }
 
-    public E removeFirst(){
-        if(size()==0) throw new UserNoSuchElementException();
+    public E removeFirst() {
+        if (size() == 0) throw new UserNoSuchElementException();
 
         E temp = head.ele;
         Node<E> curr = head;
@@ -102,30 +102,30 @@ public class UserLinkedList<E>  {
         return temp;
     }
 
-    public E getFirst(){
-        if(size()==0){
+    public E getFirst() {
+        if (size() == 0) {
             throw new UserNoSuchElementException();
         }
         return head.ele;
     }
 
-    public E getLast(){
-        if(size()==0){
+    public E getLast() {
+        if (size() == 0) {
             throw new UserNoSuchElementException();
         }
         return tail.ele;
     }
 
 
-    public E remove(int indx){
-        if(indx<0 || indx>=size()){
-            throw new UserIndexOutOfBoundsException("Index: " + indx+", Size "+ size());
+    public E remove(int indx) {
+        if (indx < 0 || indx >= size()) {
+            throw new UserIndexOutOfBoundsException("Index: " + indx + ", Size " + size());
         }
         //case 1 - remove head if index is 0
-        if(indx==0){
+        if (indx == 0) {
             E temp = head.ele;
             head = head.next;
-            if(size()==1){
+            if (size() == 1) {
                 tail = null;
             }
             this.indx--;
@@ -134,7 +134,7 @@ public class UserLinkedList<E>  {
 
         //case 3 - remove non-head
         Node<E> curr1 = head;
-        for(int i =0;i<indx-1; i++){
+        for (int i = 0; i < indx - 1; i++) {
             curr1 = curr1.next;
 
         }
@@ -147,56 +147,127 @@ public class UserLinkedList<E>  {
         return temp;
     }
 
-    public boolean contains(Object obj){
+    public boolean contains(Object obj) {
         E search = (E) obj;
-        
+        if (search.equals(getLast())) return true;
+        Node<E> curr = head;
+
+        while (curr.next != null) {
+            if (curr.ele.equals(search)) return true;
+            curr = curr.next;
+        }
+
+        return false;
     }
 
-    public void add(int indx, E ele){
-        if(indx<0 || indx>size()){
-            throw new UserIndexOutOfBoundsException("Index : "+indx+", Size"+ size());
+
+    public int indexOf(Object obj) {
+        E search = (E) obj;
+        if (!contains(obj)) return -1;
+
+        if (search.equals(getLast())) return size() - 1;
+
+        Node<E> curr = head;
+
+        int i = 0;
+        while (curr.next != null) {
+            if (curr.ele.equals(search)) return i;
+            i++;
+            curr = curr.next;
+        }
+        return -1;
+    }
+
+
+    public E remove() {
+        return removeLast();
+    }
+
+    public void clear() {
+        while (!isEmpty()) removeFirst();
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public void addAll(UserLinkedList<E> list) {
+        UserLinkedList<E> list2 = new UserLinkedList<>();
+        for (int i = 1; i < list.size(); i++) {
+            list2.add(list.get(i));
+        }
+    }
+
+    public E get(int indx) {
+        if (indx < 0 || indx > size()) throw new UserIndexOutOfBoundsException("index invalid ");
+        if (indx == size() - 1) return getLast();
+        int i = 1;
+        Node<E> curr = head;
+
+        while (curr.next != null) {
+            if (i == indx) {
+                return curr.ele;
+            }
+            i++;
+            curr = curr.next;
+        }
+        return null;
+    }
+
+
+    public void add(int indx, E ele) {
+        if (indx < 0 || indx > size()) {
+            throw new UserIndexOutOfBoundsException("Index : " + indx + ", Size" + size());
         }
 
         Node<E> newNode = new Node<E>(ele);
         Node<E> curr1 = head;
 
-        for(int i =0;i<indx-1;i++){
+        for (int i = 0; i < indx - 1; i++) {
             curr1 = curr1.next;
         }
 
-        Node<E>curr2 = curr1.next;
+        Node<E> curr2 = curr1.next;
 
         newNode.next = curr2;
-         curr1.next = newNode;
-         if(newNode.next == null){
-             tail = newNode;
-         }
+        curr1.next = newNode;
+        if (newNode.next == null) {
+            tail = newNode;
+        }
         this.indx++;
     }
 
 
-    public boolean isEmpty(){
-        return size()==0;
+    public boolean isEmpty() {
+        return size() == 0;
     }
 }
 
-class Main{
+class Main {
 
     static void main(String[] args) {
-    UserLinkedList<Integer> list = new UserLinkedList<>();
+        UserLinkedList<Integer> list = new UserLinkedList<>();
+//        System.out.println(list.add(19));
+//        System.out.println(list.add(15));
+//        System.out.println(list.add(123));
+//        System.out.println(list);
+//        list.addLast(23);
+//        System.out.println(list);
+//        System.out.println(list.getLast());
+//        list.addFirst(22);
+//        System.out.println(list);
 
-        System.out.println(list.add(19));
-        System.out.println(list.add(15));
-        System.out.println(list.add(123));
+        System.out.println(list.add(10));
+        System.out.println(list.add(20));
+        System.out.println(list.add(30));
+        System.out.println(list.add(40));
         System.out.println(list);
 
-        list.addLast(23);
-        System.out.println(list);
-        System.out.println(list.getLast());
-
-        list.addFirst(22);
-        System.out.println(list);
-
+        //let add an element to the index
+        list.add(1,12);
+        list.remove(1);
+        list.get(2);
     }
 
 }
