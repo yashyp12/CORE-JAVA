5c7af650d70f4b78ff24f5c6ac580ff473c0fb54
Yash Patil <patilyashyp12@gmail.com>
2026-02-03 12:49:37 +0530

    Add basic doubly linked list implementation
    Introduced a DLL class with methods to insert a node at the beginning and display the list. Added an inner Node class to represent list nodes with value, next, and previous pointers.

--- Collections-1/List/LinkedList/DLL.java
+++ Collections-1/List/LinkedList/DLL.java
@@ -1,4 +1,47 @@
 package List.LinkedList;
 
 public class DLL {
+
+    Node head;
+
+    public void insertFirst(int val){
+        Node newNode =  new Node(val); //box is created new
+
+        newNode.next = head;
+        newNode.prev = null;
+        if(head!=null) {
+            head.prev = newNode; // head maybe null if isnerting at first
+        }
+        head = newNode;
+    }
+
+    public void display(){
+        Node temp = head;
+
+        while(temp!=null){
+            System.out.print("HEAD -> " + temp.val + " ->");
+            temp = temp.next; //not chaning modifying the structure of the linked list
+        }
+
+    }
+
+
+
+
+    private class Node {
+        int val;
+        Node next;
+        Node prev;
+
+        Node(int val) {
+            this.val = val;
+        }
+
+        Node(int val, Node next, Node prev) {
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
 }
