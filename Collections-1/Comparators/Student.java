0b85627799e8c130b12ad3b82bbc43a0dc6bd17c
Yash Patil <patilyashyp12@gmail.com>
2026-02-07 14:23:51 +0530

    Add toString and demo sort in Student
    Implement Student.toString(), add Arrays import and a small demo that creates multiple Student instances, prints the array, sorts it with Arrays.sort(), and prints the result. Also rename local variable (yp->rahul) and adjust compareTo usage to reflect the new samples. Compiled class file updated accordingly.

--- Collections-1/Comparators/Student.java
+++ Collections-1/Comparators/Student.java
@@ -1,5 +1,8 @@
 package Comparators;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Student implements Comparable<Student> {
 
     int rollNo;
@@ -10,7 +13,15 @@ public class Student implements Comparable<Student> {
         this.marks = marks;
     }
 
-        @Override
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", marks=" + marks +
+                '}';
+    }
+
+    @Override
     public int compareTo(Student o) {
         int diff = (int)(this.marks - o.marks);
         //if diff == 0 means both are equal
@@ -21,10 +32,20 @@ public class Student implements Comparable<Student> {
     //comparing the objects
     static void main(String[] args) {
         Student yash = new Student(12,89.76f);
-        Student yp = new Student(15,90.76f);
+        Student rahul = new Student(15,90.76f);
+        Student shubham = new Student(16,93.76f);
+        Student rakesh = new Student(17,95.76f);
+        Student sagar = new Student(18,97.76f);
             //IF BOTH ARE EQUAL then it wil be 0
-        if(yash.compareTo(yp)<0){
-            System.out.println("yp has more marks " + yash.compareTo(yp));
+
+        Student [] list = {yash,rahul,shubham,rakesh,sagar};
+
+        System.out.println(Arrays.toString(list));
+        Arrays.sort(list);
+        System.out.println(Arrays.toString(list));
+
+        if(yash.compareTo(rahul)<0){
+            System.out.println("yp has more marks " + yash.compareTo(rahul));
         }
 
     }
